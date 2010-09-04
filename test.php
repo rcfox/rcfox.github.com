88444658d65136002609ecfaa469222c2812f6d8
Ryan Fox <foxryan@gmail.com>
2010-09-03 22:55:18 -0400

    Let's see if it will treat .php as a regular HTML file.

---
+++ test.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<!-- bcat was here --> 
+<title>/home/ryan/code/www/rcfox.github.com</title> 
+ 
+</head><body> 
+<pre>index.html<br>README<br>test.php<br></pre></body></html>
